d85b69d2fc741697fa408c0210e1c9c4b8a5ff8e
Nitish Kumar Yadav <engineer.nitish7115@gmail.com>
2023-01-21 23:18:01 +0530

    Added Responsiveness to Products Page

--- src/Components/Products/ProductsGrid/ProductItem.js
+++ src/Components/Products/ProductsGrid/ProductItem.js
@@ -6,7 +6,7 @@ import classes from "./ProductItem.module.css";
 const ProductItem = (props) => {
   return (
     <Fragment>
-      <div className={classes.itemWrapper}>
+      {/* <div className={classes.itemWrapper}>
         <div className={classes.productImg}>
           <Link to={`${props.id}`}>
             <img src={props.image} alt={props.title} />
@@ -24,6 +24,27 @@ const ProductItem = (props) => {
           <div className={classes.price}>
             <li>{props.price}</li>
           </div>
+        </div>
+      </div> */}
+
+      <div className={classes.itemWrapper}>
+        <div className={classes.imgContainer}>
+          <Link to={`${props.id}`} className={classes.imgLink}>
+            <img
+              src={props.image}
+              alt={props.title}
+              className={classes.productImg}
+            />
+          </Link>
+          {/* <hr /> */}
+        </div>
+
+        <div className={classes.productData}>
+          <li className={classes.title}>
+            <Link to={`${props.id}`}>{props.title}</Link>
+          </li>
+          <li>{props.category}</li>
+          <li>{props.price}</li>
         </div>
       </div>
     </Fragment>
